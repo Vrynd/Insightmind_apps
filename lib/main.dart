94648d4834bdd27e3f27f1dcfd456ffb03fc5245
Vrynd <vryndhika@gmail.com>
2025-11-06 15:52:09 +0700

    Menambahkan depedensi dan model untuk riwayat

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:insightmind_app/features/insightmind/data/local/screening_record.dart';
 import 'package:insightmind_app/src/app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(ScreeningRecordAdapter());
+  await Hive.openBox<ScreeningRecord>('screening_records');
+
   runApp(const ProviderScope(child: InsightMindApp()));
 }
